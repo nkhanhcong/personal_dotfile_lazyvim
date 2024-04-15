ccbc6ef92ad1e36093a8a7e2ff4810ae96080bea
congnk <nkhanhcong@gmail.com>
2024-04-15 08:56:49 +0700

    add config dot

---
+++ lua/plugins/surrounds.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"echasnovski/mini.surround",
+		opts = {
+			mappings = {
+				delete = "sd", -- Delete surrounding
+				add = "sa",
+				replace = "sr",
+				highlight = "sh",
+			},
+		},
+	},
+}
